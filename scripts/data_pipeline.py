34d97a88ec772c842e24163365d3ebbfa2ef8f3f
brunolimabh <bruno.lima@sptech.school>
2025-10-14 17:45:17 -0300

    feat

--- scripts/data_pipeline.py
+++ scripts/data_pipeline.py
@@ -1,15 +1,19 @@
 import boto3, pandas as pd, os
-bucket = "lab-sprint5-arqnuvem-891377383993"
+
+bucket = os.environ["BUCKET_NAME"]
 s3 = boto3.client("s3")
 local = "data/sales.csv"
+
 # RAW
 s3.upload_file(local, bucket, "raw/sales.csv")
+
 # TRUSTED
 df = pd.read_csv(local).dropna()
 df.columns = [c.strip().lower().replace(" ", "_") for c in
 df.columns]
 df.to_csv("trusted_sales.csv", index=False)
 s3.upload_file("trusted_sales.csv", bucket, "trusted/sales.csv")
+
 # CLIENT
 client = df[['order_id','amount']]
 client.to_csv("client_sales.csv", index=False)
